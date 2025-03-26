6a650da11f8e30e32a241f5f307fb64f6bb5245a
Raphael Ho <raphaeldominique@gmail.com>
2025-03-26 14:47:23 -0600

    Testing with beautiful soup
    Tried using some parts of beautiful soup

--- facebook.py
+++ facebook.py
@@ -2,11 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+
 from login import NAME, GMAIL, PASSWORD
 
 import time
+import os
+
+
 
 def open():
+    os.system("cls")
     open_options = Options()
 
     # Opens page until click close yourself, can delete later
@@ -34,4 +40,22 @@ def open():
 
     password_enter.send_keys(Keys.ENTER)
 
-open()
+    html_content = driver.page_source
+
+    return html_content
+
+def traverse(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    title = soup.find("div", class_ = "x9f619 x78zum5 x1r8uery xdt5ytf x1iyjqo2 xs83m0k x1e558r4 x150jy0e x1iorvi4 xjkvuk6 xnpuxes x291uyu x1uepa24")
+    
+    
+    print(title)
+    print("bbbbbb")
+
+def main():
+    html_content = open()
+    traverse(html_content)
+
+if __name__ == "__main__":
+    main()
